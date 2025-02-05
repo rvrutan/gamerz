e83ca9a8ab0305aa7208193b400728b4f95f0fe6
rvrutan <rutanroni@gmail.com>
2025-02-05 10:16:24 -0500

    tailwind added, daisyui implemented, footer fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,26 @@
 export default function Footer() {
-    return (
-    <footer className="footer p-4 bg-gray-800 text-white text-center">
-      <p>Teammates: 
-        <a href="https://github.com/rvrutan" className="mx-2">Roni Yalin</a> | 
-        <a href="https://github.com/SamanthaMakowski" className="mx-2">Samantha Makowski</a> |
-        <a href="https://github.com/Lindseyt75" className="mx-2">Timothy Lindsey</a> | 
-        <a href="https://github.com/xBenCookx" className="mx-2">Ben Cook</a>
-      </p>
+  return (
+    <footer className="footer p-4 bg-neutral text-neutral-content text-center">
+      <div className="container mx-auto">
+        <p>
+          Colabborators: 
+          <a href="https://github.com/rvrutan" className="mx-2 link link-hover">
+            Roni v. Rutan
+          </a> 
+          | 
+          <a href="https://github.com/SamanthaMakowski" className="mx-2 link link-hover">
+            Samantha Makowski
+          </a> 
+          | 
+          <a href="https://github.com/Lindseyt75" className="mx-2 link link-hover">
+            Timothy Lindsey
+          </a> 
+          | 
+          <a href="https://github.com/xBenCookx" className="mx-2 link link-hover">
+            Ben Cook
+          </a>
+        </p>
+      </div>
     </footer>
-  )};
+  );
+}
