47eb298755fc61a02abe635286b911a16bfbbcdf
rvrutan <rutanroni@gmail.com>
2025-02-11 00:01:32 -0500

    allowed hosts added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,34 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    // allowedHosts: ['gamerz-client.onrender.com'],
-    host: '0.0.0.0',
+    allowedHosts: ["gamerz-client.onrender.com", "gamerz.onrender.com"],
+    host: "0.0.0.0",
     port: 3001,
     open: true,
     proxy: {
-      '/api': {
+      "/api": {
         //test local http://localhost:3001
-        target: 'https://gamerz.onrender.com/',
+        target: "https://gamerz.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
-      '/auth': {
-                //test local http://localhost:3001
-        target: 'https://gamerz.onrender.com/',
+      "/auth": {
+        //test local http://localhost:3001
+        target: "https://gamerz.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-  plugins: [tailwindcss(),
-    react()],
-    preview: {
-      host: true,
-      port: 3000
-    }
-  
-})
+  plugins: [tailwindcss(), react()],
+  preview: {
+    host: true,
+    port: 3000,
+  },
+});
